c731f9e143b119242831cfbb698524b56c1273b5
AnhPd <phamdacanh2k2@gmail.com>
2024-11-03 17:46:24 +0700

    - change model in home

---
+++ Assets/GamePlay/Scripts/StateMachine/Shop/DisplayCharacter.cs
@@ -0,0 +1,13 @@
+using StateMachineNP;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayCharacter : MonoBehaviour
+{
+    [SerializeField] Animator anim;
+    private void Start()
+    {
+
+    }
+}
